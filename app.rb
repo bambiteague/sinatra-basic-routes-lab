c2eb0927ed23bff8cc004ff23e11179cdbe6ec61
bambi teague <teeegsa@gmail.com>
2021-02-03 16:12:59 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do 
+    "My name is Bambi James Teague"
+  end
+
+  get '/hometown' do
+    "My hometown is Arlington, TX"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is 'Tangerine' by The Glass Animals (right now, anyways)"
+  end
+
 end
